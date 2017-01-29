7fdacad0372d45adb9d2de2c4f15c856ca1a2d5d
shtigran <shakhbekyantigran@gmail.com>
2017-01-29 14:28:21 +0400

    LINQ to Object by var

--- LinqToObject/LinqToObject/Program.cs
+++ LinqToObject/LinqToObject/Program.cs
@@ -14,13 +14,13 @@ namespace LinqToObject
             Console.WriteLine(new string('-', 30)+"\n");
 
             QueryToArray();
-
+            VarQuery();
 
 
             Console.ReadKey();
         }
 
-        // LINQ to Array of strings
+        // LINQ to Array of strings by  IEnumerable<>
         static void QueryToArray()
         {
             string[] FootballTeams = { "Real Madrid", "Barcelona", "AC Milan", "Mancester United", "Shirak", "Mancester City"};
@@ -29,9 +29,20 @@ namespace LinqToObject
                                          where g.Contains(" ") && (g.StartsWith("M") || g.StartsWith("R"))
                                          orderby g
                                          select g;
-            Console.WriteLine("Result of LINQ to Array\n");
+            Console.WriteLine("Result of LINQ to Array by  IEnumerable<>\n");
             foreach (string s in subset)
                 Console.WriteLine($"Team: {s}");
+        }
+
+        //LINQ to Array by Implicitly Typed Local Variables  
+        static void VarQuery()
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7, 9, 10 };
+            
+            var subset = from i in numbers where i%2==0 select i;
+            Console.WriteLine("\nResult of LINQ to Array by var\n");
+            foreach (var i in subset)
+                Console.WriteLine($"Number: {i} ");
         }
     }
 }
